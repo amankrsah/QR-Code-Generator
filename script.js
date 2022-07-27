175bca3a3cd40e79f885cf3eb9aeed2790650be8
amankrsah <69616665+amankrsah@users.noreply.github.com>
2022-07-28 00:27:48 +0530

    fixed alert bug

--- script.js
+++ script.js
@@ -8,13 +8,12 @@ let alertmsg = document.querySelector(".alertmsg");
 let dismissBtn = document.querySelector(".dismiss_btn");
 
 function dismiss() {
-    alert.classList.remove("animateAlert")
+    alert.setAttribute("style", "top:-200px")
     alert.classList.add("removealerts")
-    alert.setAttribute("style", "top:-500px")
     setTimeout(() => {
+        alert.setAttribute("style", "display:none")
         alert.classList.remove("removealerts")
-
-    }, 100);
+    }, 100)
     console.log("Function Called..!")
 }
 
@@ -24,13 +23,20 @@ function alerts(p) {
         document.querySelector(".dismiss_btn").setAttribute("style", "display:block;")
         alert.setAttribute("style", "background-color:red;top:5px")
         alert.classList.add("animateAlert")
+            // setTimeout(() => {
+            //     alert.classList.remove("animateAlert")
+            // }, 300);
 
     } else if (p == 's') {
 
         alertmsg.innerText = "QR-code Generated successfully..💯"
         alert.setAttribute("style", "background-color:rgb(0 163 86);top:5px")
         document.querySelector(".container").classList.add("active")
+        alert.setAttribute("style", "display:block")
         alert.classList.add("animateAlert")
+        setTimeout(() => {
+            alert.classList.remove("animateAlert")
+        }, 200)
 
 
     }
